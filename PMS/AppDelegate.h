90dce1c4676240acf26a74a90bbf029bf8dfb35c
longdq <longbalan@gmail.com>
2015-10-30 17:47:47 +0700

    #1001 setup

---
+++ PMS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PMS
+//
+//  Created by longdq on 10/30/15.
+//  Copyright © 2015 longdq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PMS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PMS
+//
+//  Created by longdq on 10/30/15.
+//  Copyright © 2015 longdq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
